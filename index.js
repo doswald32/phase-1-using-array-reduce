2d43cc61dcfe33622aefc0aac957c0a063a1650d
doswald32 <dan.oswald32@gmail.com>
2024-01-06 13:25:49 -0700

    again

--- index.js
+++ index.js
@@ -3,5 +3,3 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 const totalBatteries = batteryBatches.reduce(function(accumulator, element) {
     return element + accumulator;
 }, 0)
-
-console.log(totalBatteries);
